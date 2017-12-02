634a55e6ad0d1ad6ef9cd5324c7361b9353a2a2a
Server <tilesmaster@tiless-mbp.attlocal.net>
2017-12-02 07:59:55 -0500

    initial commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
+
+module.exports = twitterKeys;
