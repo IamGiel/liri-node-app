5a4ce8ff6223f37459f84a8869cf6b292bb144c3
Server <tilesmaster@tiless-mbp.attlocal.net>
2017-12-02 21:23:58 -0500

    added twitter  keys

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'qK3kn3xdPzcLob2HDDoyFuztU',
+  consumer_secret: 'lUUTycgoo1yfDXTo8GhJT9HNxC8rPTJBZrcFw74LekF0T91ss2',
+  access_token_key: '936931978271776768-t7fMJyHkjODgEz2CKZXJCCxJi5HJCWg',
+  access_token_secret: 'GKg54rLCkLGXF0IWRIvm5ijb5ckjeCiy0DWiaiAqsZX38',
 }
 
 module.exports = twitterKeys;
