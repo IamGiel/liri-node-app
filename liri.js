3f14247789d7a3ebe214bf0250a0f18319a9b0bc
Server <tilesmaster@Tiless-MacBook-Pro.local>
2017-12-04 18:01:40 -0500

    edit response

--- liri.js
+++ liri.js
@@ -5,9 +5,10 @@
 	var twitter = require("twitter");
 	var spotify = require ("spotify");
 	var liriArgument = process.argv[2];
-// ---------------------------------------------------------------------------------------------------------------
-	// Possible commands for this liri app
+	
 
+	//=================================================================
+	// Possible commands for this liri app
 	switch(liriArgument) {
 		case "my-tweets": myTweets(); break;
 		case "spotify-this-song": spotifyThisSong(); break;
@@ -21,7 +22,8 @@
 			"\n OPTION: 4. do-what-it-says."+
 			"\nBe sure to put the movie or song name in quotation marks if it's more than one word.");
 	};
-// ---------------------------------------------------------------------------------------------------------------
+
+	//=================================================================
 	// Movie function, uses the Request module to call the OMDB api
 	function movieThis(){
 		var movie = process.argv[3];
@@ -55,46 +57,90 @@
 			}
 		});
 	};
-	// Tweet function, uses the Twitter module to call the Twitter api
-	function myTweets() {
-		var client = new twitter({
-			//Add here!
-			oauth: {
-				twitter: {
-				consumer_key: "u5L64CZk3QLvKMfFJfkHoKVsY", // REQUIRED
-				consumer_secret: "JO25nuQDnCyYetIM1bfG01tU46z8yV9PG62ZLgHrmOfQ9oJvUV" // REQUIRED
-				}
-			}
 
+	//=================================================================
+	// Tweet function, uses the Twitter module to call the Twitter api
+	function myTweets() { 
+		var client = new twitter({
+		  consumer_key: 'u5L64CZk3QLvKMfFJfkHoKVsY',
+		  consumer_secret: 'JO25nuQDnCyYetIM1bfG01tU46z8yV9PG62ZLgHrmOfQ9oJvUV',
+		  access_token_key: '937407163324162054-T02uzHa1i4fB5yJHK7VTzbOmzXFyKFI',
+		  access_token_secret: '4jy5h7WipMyxdBYlAkqoIdRRFp6hjw5c34Zhn2uGrm6Du'
 		});
-		// var client = new twitter({
-		// 	consumer_key: keys.twitterKeys.consumer_key,
-		// 	consumer_secret: keys.twitterKeys.consumer_secret,
-		// 	access_token_key: keys.twitterKeys.access_token_key,
-		// 	access_token_secret: keys.twitterKeys.access_token_secret, 
-		// });
-		
+		 
 		var twitterUsername = process.argv[3];
+		var text = "text";
+		var params = {screen_name: twitterUsername, count: 20};
 		if(!twitterUsername){
 			twitterUsername = "LfaLeg";
 		}
-		params = {screen_name: twitterUsername};
-		client.get("statuses/user_timeline/", params, function(error, data, response){
-			if (!error) {
-				for(var i = 0; i < data.length; i++) {
-					console.log(response); // Show the full response in the terminal
-					var twitterResults = 
-					"@" + data[i].user.screen_name + ": " + 
-					data[i].text + "\n" + 
-					data[i].created_at + "\n" + 
-					"------------------------------ " + i + " ------------------------------" + "\r\n";
-					console.log(twitterResults);
-					log(twitterResults); // calling log function
-				}
-			}  else {
-				console.log("Error :"+ error);
-				return;
-			}
-		});
+		client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		  if (!error) {
+		  	for (var i = 0; i < tweets.length; i++) {
+		  		console.log(response.text);
+		  		console.log(tweets.text);
+		  		console.log(JSON.stringify(response.text, null, 2));
+
+		  		// console.log(response.text);
+		  		console.log("------------------------"+  i  +"---------------------------");
+		  	}
+		  	for (var i = 0; i < tweets.length; i++) {
+		  		console.log(response.text);
+		  		console.log(tweets.text);
+		  		console.log(JSON.stringify(response.text, null, 2));
+
+		  		// console.log(response.text);
+		  		console.log("------------------------"+  i  +"---------------------------");
+		  	}
+		  }
+		});	
 	}
-	
+
+	//=================================================================
+	// // Spotify function, uses the Spotify module to call the Spotify api
+	// function spotifyThisSong(songName) {
+	// 	var songName = process.argv[3];
+	// 	if(!songName){
+	// 		songName = "What's my age again";
+	// 	}
+	// 	params = songName;
+	// 	spotify.search({ type: "track", query: params }, function(err, data) {
+	// 		if(!err){
+	// 			var songInfo = data.tracks.items;
+	// 			for (var i = 0; i < 5; i++) {
+	// 				if (songInfo[i] != undefined) {
+	// 					var spotifyResults =
+	// 					"Artist: " + songInfo[i].artists[0].name + "\r\n" +
+	// 					"Song: " + songInfo[i].name + "\r\n" +
+	// 					"Album the song is from: " + songInfo[i].album.name + "\r\n" +
+	// 					"Preview Url: " + songInfo[i].preview_url + "\r\n" + 
+	// 					"------------------------------ " + i + " ------------------------------" + "\r\n";
+	// 					console.log(spotifyResults);
+	// 					log(spotifyResults); // calling log function
+	// 				}
+	// 			}
+	// 		}	else {
+	// 			console.log("Error :"+ err);
+	// 			return;
+	// 		}
+	// 	});
+	// };
+	// // Do What It Says function, uses the reads and writes module to access the random.txt file and do what's written in it
+	// function doWhatItSays() {
+	// 	fs.readFile("random.txt", "utf8", function(error, data){
+	// 		if (!error) {
+	// 			doWhatItSaysResults = data.split(",");
+	// 			spotifyThisSong(doWhatItSaysResults[0], doWhatItSaysResults[1]);
+	// 		} else {
+	// 			console.log("Error occurred" + error);
+	// 		}
+	// 	});
+	// };
+	// // Do What It Says function, uses the reads and writes module to access the log.txt file and write everything that returns in terminal in the log.txt file
+	// function log(logResults) {
+	//   fs.appendFile("log.txt", logResults, (error) => {
+	//     if(error) {
+	//       throw error;
+	//     }
+	//   });
+	// }
