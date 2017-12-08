f83762a4b01213b36b738c6a6a84bcf3840ea72a
Server <tilesmaster@tiless-mbp.attlocal.net>
2017-12-08 11:05:17 -0500

    clean/edit

--- liri.js
+++ liri.js
@@ -8,21 +8,21 @@
 	var userINPUT = process.argv[3];
 	
 	
-		//=================================================================
+	//=================================================================
 	// Possible commands for this liri app
 	
-		switch(liriArgument) {
-			case "my-tweets": myTweets(); break;
-			case "spotify-this-song": spotifyThisSong(); break;
-			case "movie-this": movieThis(); break;
-			case "do-what-it-says": doWhatItSays(); break;
-			// Instructions displayed in terminal to the user
-			default: console.log("=========== MY NAME IS LIRI, CHOOSE ONE OF OPTION COMMANDS BELOW: ==============\n" + 
-				"\n OPTION: 1. my-tweets 'any twitter name' " +
-				"\n OPTION: 2. spotify-this-song 'any song name' "+
-				"\n OPTION: 3. movie-this 'any movie name' "+
-				"\n OPTION: 4. do-what-it-says."+ "\n\n" +
-				"\n**********\nBe sure to put the movie or song name in quotation \nmarks if it's more than one word.\n**********\n\n\n");
+	switch(liriArgument) {
+		case "my-tweets": myTweets(); break;
+		case "spotify-this-song": spotifyThisSong(); break;
+		case "movie-this": movieThis(); break;
+		case "do-what-it-says": doWhatItSays(); break;
+		// Instructions displayed in terminal to the user
+		default: console.log("=========== MY NAME IS LIRI, CHOOSE ONE OF OPTION COMMANDS BELOW: ==============\n" + 
+			"\n OPTION: 1. my-tweets 'any twitter name' " +
+			"\n OPTION: 2. spotify-this-song 'any song name' "+
+			"\n OPTION: 3. movie-this 'any movie name' "+
+			"\n OPTION: 4. do-what-it-says."+ "\n\n" +
+			"\n**********\nBe sure to put the movie or song name in quotation \nmarks if it's more than one word.\n**********\n\n\n");
 	}
 	
 	//=================================================================
@@ -114,24 +114,23 @@
 		var songName = userINPUT;
 		var space = "\n" + "\n" +"\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0";
 		if(!songName){
-			 		songName = "What's my age again";
-				}
+			SongName = "What's my age again";
+		}
+
 		params = songName;
-		
-		
 		spotify.search({ type: 'track', query: params }, function(err, data) {
 			if ( err ) {
 			    console.log('Error occurred: ' + err);
 			    return;  
 			}
 			else{
-					output = space + "================= LIRI FOUND THIS FOR YOU...==================" + 
-					space + "Song Name: " + "'" +songName.toUpperCase()+ "'" +
-					space + "Album Name: " + data.tracks.items[0].album.name +
-					space + "Artist Name: " + data.tracks.items[0].album.artists[0].name +	
-					space + "URL: " + data.tracks.items[0].album.external_urls.spotify + "\n\n\n";
-
-					console.log(output);
+				output = space + "================= LIRI FOUND THIS FOR YOU...==================" + 
+				space + "Song Name: " + "'" +songName.toUpperCase()+ "'" +
+				space + "Album Name: " + data.tracks.items[0].album.name +
+				space + "Artist Name: " + data.tracks.items[0].album.artists[0].name +	
+				space + "URL: " + data.tracks.items[0].album.external_urls.spotify + "\n\n\n";
+				console.log(output);
+					
 					fs.appendFile("log.txt", output, function (err) {
 					  if (err) throw err;
 					  console.log('Saved!');
@@ -153,30 +152,30 @@
 
 			var command;
 			var parameter;
-			
-	        command = loggedTxt[0];
-	        parameter = loggedTxt[1];
 
-		   parameter = parameter.replace('"', '');
-	       parameter = parameter.replace('"', '');
-	       // console.log(parameter);
+			command = loggedTxt[0];
+			parameter = loggedTxt[1];
 
-	       switch (command) {
-	           case 'my-tweets':
-	               userINPUT = parameter;
-	               myTweets();
-	               break;
+			parameter = parameter.replace('"', '');
+			parameter = parameter.replace('"', '');
+			// console.log(parameter);
 
-	           case 'spotify-this-song':
-	               userINPUT = parameter;
-	               spotifyThisSong();
-	               break;
+			switch (command) {
+			   case 'my-tweets':
+			       userINPUT = parameter;
+			       myTweets();
+			       break;
 
-	           case 'movie-this':
-	               userINPUT = parameter;
-	               movieThis();
-	               break;
-	       }
+			   case 'spotify-this-song':
+			       userINPUT = parameter;
+			       spotifyThisSong();
+			       break;
+
+			   case 'movie-this':
+			       userINPUT = parameter;
+			       movieThis();
+			       break;
+			}
 		});
 
 	}
