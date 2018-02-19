4c5e6734e6dfcff4c70521f7e57845fd56790182
Server <november14giel@gmail.com>
2018-02-19 10:30:29 -0500

    git

--- liri.js
+++ liri.js
@@ -67,12 +67,7 @@
 	//=================================================================
 	// Tweet function, Twitter api
 	function myTweets() { 
-		var client = new twitter({
-		  consumer_key: 'u5L64CZk3QLvKMfFJfkHoKVsY',
-		  consumer_secret: 'JO25nuQDnCyYetIM1bfG01tU46z8yV9PG62ZLgHrmOfQ9oJvUV',
-		  access_token_key: '937407163324162054-T02uzHa1i4fB5yJHK7VTzbOmzXFyKFI',
-		  access_token_secret: '4jy5h7WipMyxdBYlAkqoIdRRFp6hjw5c34Zhn2uGrm6Du'
-		});
+		var client = new twitter(keys.twitterKeys);
 		 
 		var twitterUsername = userINPUT;
 		var text = "text";
